d0406ad52985b8477d7a88ede287fcdfb25fcf6d
alansuface <alansuface@192.168.86.33>
2020-05-10 23:51:20 -0700

    started my wumpus hunter subclass

---
+++ WumpusMountain/src/MyWumpusHunter.java
@@ -0,0 +1,14 @@
+
+public class MyWumpusHunter extends WumpusHunter {
+
+	public MyWumpusHunter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void startAt(MountainCave root) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
