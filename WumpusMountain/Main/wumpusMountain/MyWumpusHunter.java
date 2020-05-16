55db3704c2d8ad158368cdd973c5464687d602c6
alansuface <alansuface@192.168.86.33>
2020-05-15 23:38:54 -0700

    My wumpus hunter searches different paths each time
    Issues are still there

--- WumpusMountain/Main/wumpusMountain/MyWumpusHunter.java
+++ WumpusMountain/Main/wumpusMountain/MyWumpusHunter.java
@@ -1,5 +1,4 @@
 package wumpusMountain;
-
 import java.util.*;
 
 public class MyWumpusHunter extends WumpusHunter {
@@ -14,21 +13,32 @@ public class MyWumpusHunter extends WumpusHunter {
 	
 	@Override
 	public void startAt(MountainCave root) {
+		//initialize a stack to store cave names
 		Stack<String> caves = new Stack<>();
+		//initialize a String variable to store the cave name
 		String location = "";
+		//initialize a random object to pick the following path
+		Random rand = new Random();
 		while(!root.hasScales()) {
-			location = root.getCaveName();
-			caves.push(location);
-			this.actionLog += "Enterting the" + location + "/n";
+			// TODO check if scales are here
+			location = root.getCaveName();//stores cave name in location variable
+			// TODO get cave name
+			caves.push(location);//store the current cave name in the stack
+			this.actionLog += "Enterting the" + location + "/n";//puts location in actionlog
+			// TODO create stack to store cave name and store in action log also
+			System.out.println(location);//
+			// TODO search for adjacent cave name
 			ArrayList<MountainCave> paths = root.getChildren();
-			if(paths.size()>0) {
-				root = paths.get(0);
+			int routes = paths.size();
+			if(routes>0) {
+				root = paths.get(rand.nextInt(routes));
+			}else if(routes == 0){
+				root = root.getParent();
+				//TODO at dead end it returns to the parent, need to search new routes
 			}
 			paths.clear();
-		}
-		// TODO create stack to store cave name and store in action log also
-		// TODO get cave name and search for adjacent cave name
-		// TODO check if scales are here
+			//TODO need to add case for when scales are at adjacent cave
+		}//TODO case if scales are found
 		// TODO if treasure is found start unloading stack
 	}
 	
